4820b707503b59cfb87df6002036f9f76c4b04c7
Calvin Tsang <debug.tsang@gmail.com>
2015-07-12 23:03:06 +0800

    print link

--- python.org_search.py
+++ python.org_search.py
@@ -1,6 +1,13 @@
+# This program demo how to use selenium and grab all urls
+# Loating elements
+# http://selenium-python.readthedocs.org/en/latest/locating-elements.html
+# Date: 2015-07-12 21:00
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 driver = webdriver.Firefox()
 driver.get("http://www.google.com")
@@ -9,9 +16,8 @@ elem = driver.find_element_by_name("q")
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
-
+wait = WebDriverWait(driver, 2)
 continue_link = driver.find_elements_by_link_text('')
+print continue_link[0].get_attribute("href");
 print( "number of links={length}").format(length = len(continue_link))
-#print ("bcd {number} rather").format(number = 3)
-
 driver.close()
